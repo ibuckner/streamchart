03f32e3ca6d0431e8634174827869a60d54c6ccf
ibuckner <ibuckner@work>
2021-04-23 13:32:11 +0100

    v0.3.2

--- docs/lib/streamchart.js
+++ docs/lib/streamchart.js
@@ -105,16 +105,18 @@ var chart = (function (exports) {
     if ((step = tickIncrement(start, stop, count)) === 0 || !isFinite(step)) return [];
 
     if (step > 0) {
-      start = Math.ceil(start / step);
-      stop = Math.floor(stop / step);
-      ticks = new Array(n = Math.ceil(stop - start + 1));
-      while (++i < n) ticks[i] = (start + i) * step;
+      let r0 = Math.round(start / step), r1 = Math.round(stop / step);
+      if (r0 * step < start) ++r0;
+      if (r1 * step > stop) --r1;
+      ticks = new Array(n = r1 - r0 + 1);
+      while (++i < n) ticks[i] = (r0 + i) * step;
     } else {
       step = -step;
-      start = Math.ceil(start * step);
-      stop = Math.floor(stop * step);
-      ticks = new Array(n = Math.ceil(stop - start + 1));
-      while (++i < n) ticks[i] = (start + i) / step;
+      let r0 = Math.round(start * step), r1 = Math.round(stop * step);
+      if (r0 / step < start) ++r0;
+      if (r1 / step > stop) --r1;
+      ticks = new Array(n = r1 - r0 + 1);
+      while (++i < n) ticks[i] = (r0 + i) / step;
     }
 
     if (reverse) ticks.reverse();
@@ -1911,7 +1913,7 @@ var chart = (function (exports) {
     }
 
     function scale(x) {
-      return isNaN(x = +x) ? unknown : (output || (output = piecewise(domain.map(transform), range, interpolate$1)))(transform(clamp(x)));
+      return x == null || isNaN(x = +x) ? unknown : (output || (output = piecewise(domain.map(transform), range, interpolate$1)))(transform(clamp(x)));
     }
 
     scale.invert = function(y) {
@@ -2484,38 +2486,40 @@ var chart = (function (exports) {
     });
   };
 
-  var durationSecond$1 = 1e3;
-  var durationMinute$1 = 6e4;
-  var durationHour$1 = 36e5;
-  var durationDay$1 = 864e5;
-  var durationWeek$1 = 6048e5;
+  const durationSecond = 1000;
+  const durationMinute = durationSecond * 60;
+  const durationHour = durationMinute * 60;
+  const durationDay = durationHour * 24;
+  const durationWeek = durationDay * 7;
+  const durationMonth = durationDay * 30;
+  const durationYear = durationDay * 365;
 
   var second = newInterval(function(date) {
     date.setTime(date - date.getMilliseconds());
   }, function(date, step) {
-    date.setTime(+date + step * durationSecond$1);
+    date.setTime(+date + step * durationSecond);
   }, function(start, end) {
-    return (end - start) / durationSecond$1;
+    return (end - start) / durationSecond;
   }, function(date) {
     return date.getUTCSeconds();
   });
 
   var minute = newInterval(function(date) {
-    date.setTime(date - date.getMilliseconds() - date.getSeconds() * durationSecond$1);
+    date.setTime(date - date.getMilliseconds() - date.getSeconds() * durationSecond);
   }, function(date, step) {
-    date.setTime(+date + step * durationMinute$1);
+    date.setTime(+date + step * durationMinute);
   }, function(start, end) {
-    return (end - start) / durationMinute$1;
+    return (end - start) / durationMinute;
   }, function(date) {
     return date.getMinutes();
   });
 
   var hour = newInterval(function(date) {
-    date.setTime(date - date.getMilliseconds() - date.getSeconds() * durationSecond$1 - date.getMinutes() * durationMinute$1);
+    date.setTime(date - date.getMilliseconds() - date.getSeconds() * durationSecond - date.getMinutes() * durationMinute);
   }, function(date, step) {
-    date.setTime(+date + step * durationHour$1);
+    date.setTime(+date + step * durationHour);
   }, function(start, end) {
-    return (end - start) / durationHour$1;
+    return (end - start) / durationHour;
   }, function(date) {
     return date.getHours();
   });
@@ -2523,7 +2527,7 @@ var chart = (function (exports) {
   var day = newInterval(
     date => date.setHours(0, 0, 0, 0),
     (date, step) => date.setDate(date.getDate() + step),
-    (start, end) => (end - start - (end.getTimezoneOffset() - start.getTimezoneOffset()) * durationMinute$1) / durationDay$1,
+    (start, end) => (end - start - (end.getTimezoneOffset() - start.getTimezoneOffset()) * durationMinute) / durationDay,
     date => date.getDate() - 1
   );
 
@@ -2534,7 +2538,7 @@ var chart = (function (exports) {
     }, function(date, step) {
       date.setDate(date.getDate() + step * 7);
     }, function(start, end) {
-      return (end - start - (end.getTimezoneOffset() - start.getTimezoneOffset()) * durationMinute$1) / durationWeek$1;
+      return (end - start - (end.getTimezoneOffset() - start.getTimezoneOffset()) * durationMinute) / durationWeek;
     });
   }
 
@@ -2584,7 +2588,7 @@ var chart = (function (exports) {
   }, function(date, step) {
     date.setUTCDate(date.getUTCDate() + step);
   }, function(start, end) {
-    return (end - start) / durationDay$1;
+    return (end - start) / durationDay;
   }, function(date) {
     return date.getUTCDate() - 1;
   });
@@ -2596,7 +2600,7 @@ var chart = (function (exports) {
     }, function(date, step) {
       date.setUTCDate(date.getUTCDate() + step * 7);
     }, function(start, end) {
-      return (end - start) / durationWeek$1;
+      return (end - start) / durationWeek;
     });
   }
 
@@ -2629,6 +2633,50 @@ var chart = (function (exports) {
       date.setUTCFullYear(date.getUTCFullYear() + step * k);
     });
   };
+
+  function ticker(year, month, week, day, hour, minute) {
+
+    const tickIntervals = [
+      [second,  1,      durationSecond],
+      [second,  5,  5 * durationSecond],
+      [second, 15, 15 * durationSecond],
+      [second, 30, 30 * durationSecond],
+      [minute,  1,      durationMinute],
+      [minute,  5,  5 * durationMinute],
+      [minute, 15, 15 * durationMinute],
+      [minute, 30, 30 * durationMinute],
+      [  hour,  1,      durationHour  ],
+      [  hour,  3,  3 * durationHour  ],
+      [  hour,  6,  6 * durationHour  ],
+      [  hour, 12, 12 * durationHour  ],
+      [   day,  1,      durationDay   ],
+      [   day,  2,  2 * durationDay   ],
+      [  week,  1,      durationWeek  ],
+      [ month,  1,      durationMonth ],
+      [ month,  3,  3 * durationMonth ],
+      [  year,  1,      durationYear  ]
+    ];
+
+    function ticks(start, stop, count) {
+      const reverse = stop < start;
+      if (reverse) [start, stop] = [stop, start];
+      const interval = count && typeof count.range === "function" ? count : tickInterval(start, stop, count);
+      const ticks = interval ? interval.range(start, +stop + 1) : []; // inclusive stop
+      return reverse ? ticks.reverse() : ticks;
+    }
+
+    function tickInterval(start, stop, count) {
+      const target = Math.abs(stop - start) / count;
+      const i = bisector(([,, step]) => step).right(tickIntervals, target);
+      if (i === tickIntervals.length) return year.every(tickStep(start / durationYear, stop / durationYear, count));
+      if (i === 0) return millisecond.every(Math.max(tickStep(start, stop, count), 1));
+      const [t, step] = tickIntervals[target / tickIntervals[i - 1][2] < tickIntervals[i][2] / target ? i - 1 : i];
+      return t.every(step);
+    }
+
+    return [ticks, tickInterval];
+  }
+  const [timeTicks, timeTickInterval] = ticker(year, month, sunday, day, hour, minute);
 
   function localDate(d) {
     if (0 <= d.y && d.y < 100) {
@@ -3338,14 +3386,6 @@ var chart = (function (exports) {
     return locale;
   }
 
-  var durationSecond = 1000,
-      durationMinute = durationSecond * 60,
-      durationHour = durationMinute * 60,
-      durationDay = durationHour * 24,
-      durationWeek = durationDay * 7,
-      durationMonth = durationDay * 30,
-      durationYear = durationDay * 365;
-
   function date(t) {
     return new Date(t);
   }
@@ -3354,7 +3394,7 @@ var chart = (function (exports) {
     return t instanceof Date ? +t : +new Date(+t);
   }
 
-  function calendar(year, month, week, day, hour, minute, second, millisecond, format) {
+  function calendar(ticks, tickInterval, year, month, week, day, hour, minute, second, format) {
     var scale = continuous(),
         invert = scale.invert,
         domain = scale.domain;
@@ -3368,27 +3408,6 @@ var chart = (function (exports) {
         formatMonth = format("%B"),
         formatYear = format("%Y");
 
-    var tickIntervals = [
-      [second,  1,      durationSecond],
-      [second,  5,  5 * durationSecond],
-      [second, 15, 15 * durationSecond],
-      [second, 30, 30 * durationSecond],
-      [minute,  1,      durationMinute],
-      [minute,  5,  5 * durationMinute],
-      [minute, 15, 15 * durationMinute],
-      [minute, 30, 30 * durationMinute],
-      [  hour,  1,      durationHour  ],
-      [  hour,  3,  3 * durationHour  ],
-      [  hour,  6,  6 * durationHour  ],
-      [  hour, 12, 12 * durationHour  ],
-      [   day,  1,      durationDay   ],
-      [   day,  2,  2 * durationDay   ],
-      [  week,  1,      durationWeek  ],
-      [ month,  1,      durationMonth ],
-      [ month,  3,  3 * durationMonth ],
-      [  year,  1,      durationYear  ]
-    ];
-
     function tickFormat(date) {
       return (second(date) < date ? formatMillisecond
           : minute(date) < date ? formatSecond
@@ -3397,33 +3416,6 @@ var chart = (function (exports) {
           : month(date) < date ? (week(date) < date ? formatDay : formatWeek)
           : year(date) < date ? formatMonth
           : formatYear)(date);
-    }
-
-    function tickInterval(interval, start, stop) {
-      if (interval == null) interval = 10;
-
-      // If a desired tick count is specified, pick a reasonable tick interval
-      // based on the extent of the domain and a rough estimate of tick size.
-      // Otherwise, assume interval is already a time interval and use it.
-      if (typeof interval === "number") {
-        var target = Math.abs(stop - start) / interval,
-            i = bisector(function(i) { return i[2]; }).right(tickIntervals, target),
-            step;
-        if (i === tickIntervals.length) {
-          step = tickStep(start / durationYear, stop / durationYear, interval);
-          interval = year;
-        } else if (i) {
-          i = tickIntervals[target / tickIntervals[i - 1][2] < tickIntervals[i][2] / target ? i - 1 : i];
-          step = i[1];
-          interval = i[0];
-        } else {
-          step = Math.max(tickStep(start, stop, interval), 1);
-          interval = millisecond;
-        }
-        return interval.every(step);
-      }
-
-      return interval;
     }
 
     scale.invert = function(y) {
@@ -3435,15 +3427,8 @@ var chart = (function (exports) {
     };
 
     scale.ticks = function(interval) {
-      var d = domain(),
-          t0 = d[0],
-          t1 = d[d.length - 1],
-          r = t1 < t0,
-          t;
-      if (r) t = t0, t0 = t1, t1 = t;
-      t = tickInterval(interval, t0, t1);
-      t = t ? t.range(t0, t1 + 1) : []; // inclusive stop
-      return r ? t.reverse() : t;
+      var d = domain();
+      return ticks(d[0], d[d.length - 1], interval == null ? 10 : interval);
     };
 
     scale.tickFormat = function(count, specifier) {
@@ -3452,20 +3437,19 @@ var chart = (function (exports) {
 
     scale.nice = function(interval) {
       var d = domain();
-      return (interval = tickInterval(interval, d[0], d[d.length - 1]))
-          ? domain(nice(d, interval))
-          : scale;
+      if (!interval || typeof interval.range !== "function") interval = tickInterval(d[0], d[d.length - 1], interval == null ? 10 : interval);
+      return interval ? domain(nice(d, interval)) : scale;
     };
 
     scale.copy = function() {
-      return copy(scale, calendar(year, month, week, day, hour, minute, second, millisecond, format));
+      return copy(scale, calendar(ticks, tickInterval, year, month, week, day, hour, minute, second, format));
     };
 
     return scale;
   }
 
   function time$1() {
-    return initRange$1.apply(calendar(year, month, sunday, day, hour, minute, second, millisecond, timeFormat).domain([new Date(2000, 0, 1), new Date(2000, 0, 2)]), arguments);
+    return initRange$1.apply(calendar(timeTicks, timeTickInterval, year, month, sunday, day, hour, minute, second, timeFormat).domain([new Date(2000, 0, 1), new Date(2000, 0, 2)]), arguments);
   }
 
   const pi = Math.PI,
